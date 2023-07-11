130d58317e3170b5f8aa020d1f0518bf260aa969
kosme <casadadaista@gmail.com>
2023-07-11 09:11:25 +0200

    new changes

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ const IS_DEVELOPMENT = process.env.NODE_ENV == 'dev'
 
 const dirApp = path.join(__dirname, 'app')
 const dirAssets = path.join(__dirname, 'assets')
-const dirStyleas = path.join(__dirname, 'styles')
+const dirStyles = path.join(__dirname, 'styles')
 
-console.log(dirApp, dirAssets, dirStyles)
+// console.log(dirApp, dirAssets, dirStyles)
+module.exports = {
+     
+}
