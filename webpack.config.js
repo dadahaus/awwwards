22ae065459159ffb5c34834adc2bfffaf92acb39
kosme <casadadaista@gmail.com>
2023-07-11 09:41:58 +0200

    successful shared text file on shared

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,42 @@ const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const IS_DEVELOPMENT = process.env.NODE_ENV == 'dev'
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
 const dirAssets = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
+
 
 // console.log(dirApp, dirAssets, dirStyles)
 module.exports = {
     entry: [
         path.join(dirApp, 'index.js'),
         path.join(dirStyles, 'index.scss')
+    ], 
+    resolve: {
+        modules: [
+             dirApp,
+             dirAssets,
+             dirStyles,
+             dirNode
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+
+        new CopyWebpackPlugin ({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        })
+
     ]
 }
