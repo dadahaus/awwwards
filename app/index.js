c069d935c37cd0b8671f2e563a3e2c8a91b5ac14
kosme <casadadaista@gmail.com>
2023-07-14 10:28:57 +0200

    new progress

--- app/index.js
+++ app/index.js
@@ -1,5 +1,2 @@
-import math from 'utils/math'
-
-if (IS_DEVELOPMENT) {
-    console.log('')
-}
+import placeholder from './images/placeholder.png'
+console.log(placeholder)
--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,81 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
+const dirApp = path.join(__dirname, "app");
+const dirShared = path.join(__dirname, "shared");
+const dirStyles = path.join(__dirname, "styles");
 
-const dirApp = path.join(__dirname, 'app')
-const dirImages = path.join(__dirname, 'images')
-const dirVideos = path.join(__dirname, 'videos')
-const dirShared = path.join(__dirname, 'shared')
-const dirStyles = path.join(__dirname, 'styles')
-const dirNode = 'node_modules'
-
+const dirNode = "node_modules";
 
 // console.log(dirApp, dirAssets, dirStyles)
 module.exports = {
-    entry: [
-        path.join(dirApp, 'index.js'),
-        path.join(dirStyles, 'index.scss')
-    ], 
-    resolve: {
-        modules: [
-             dirApp,
-             dirShared,
-             dirVideos,
-             dirImages,
-             dirStyles,
-             dirNode
-        ]
-    },
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
 
-    plugins: [
-        new webpack.DefinePlugin({
-            IS_DEVELOPMENT
-        }),
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
 
-        new CopyWebpackPlugin ({
-            patterns: [
-                {
-                    from: './shared',
-                    to: ''
-                }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./shared",
+          to: "",
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: "file-loader",
+        options: {
+          name(file) {
+            return "[name].[ext]";
+          },
+        },
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: ''
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                    }
-                ]
-            },
-
-            {
-                test:  /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-                loader: 'file-loader',
-                options: {
-                    
-                }
-            }
-        ]
-    }
-}
+  },
+};
