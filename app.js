318883dd41592176a9a36b194f29123d6024d15b
ostin <casadadaista@gmail.com>
2024-03-29 19:18:04 +1100

    dotenv

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
-  
-})
+app.get("/", (req, res) => {
+  res.render("pages/home");
+});
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
-})
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
-})
+app.get("/collections", (req, res) => {
+  res.render("pages/collections");
+});
 
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail')
-})
-
-
+app.get("/detail/:uid", (req, res) => {
+  res.render("pages/detail");
+});
 
 // app.get('/', (req, res) => {
 //   res.send('<strong>Hello Worldasdasd!</strong>')
 // })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
